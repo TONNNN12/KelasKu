b5f40c6279e7859518e5bcac1e1786886a8b833d
Anton <muhammadanton340@gmail.com>
2025-07-20 20:27:41 +0700

    Login

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TugasController;
 use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
-});
-Route::get('dashboard',[DashboardController::class,'index']);
+})->name('welcome');
+// Dashboard
+Route::get('dashboard',[DashboardController::class,'index'])->name('dashboard');
+// Login
+Route::get('login',[AuthController::class,'login'])->name('login');
+// User
+Route::get('user',[UserController::class,'index'])->name('user');
+// User
+Route::get('tugas',[TugasController::class,'index'])->name('tugas');
