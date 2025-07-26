f09b4e29e0f8fae93722bf6c7353c020033805bf
Anton <muhammadanton340@gmail.com>
2025-07-26 21:25:48 +0700

    CRUD

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -89,7 +89,7 @@ class UserController extends Controller
     }
     public function destroy ($id){
         $user = User::findOrFail($id);
-        $user = delete();
+        $user->delete();
 
         return redirect()->route('user')->with('success','Data Berhasil Di Hapus');
     }
