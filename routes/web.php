5272c53d123daea9219cdd63ec1bbe5cda73c113
Anton <muhammadanton340@gmail.com>
2025-07-26 21:20:33 +0700

    CRUD

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,11 @@ Route::middleware('checkLogin')->group(function(){
 Route::get('dashboard',[DashboardController::class,'index'])->name('dashboard');
 // User
 Route::get('user',[UserController::class,'index'])->name('user');
+Route::get('user/create',[UserController::class,'create'])->name('userCreate');
+Route::post('user/store',[UserController::class,'store'])->name('userStore');
+Route::get('user/edit/{id}',[UserController::class,'edit'])->name('userEdit');
+Route::post('user/update/{id}',[UserController::class,'update'])->name('userUpdate');
+Route::delete('user/destroy/{id}',[UserController::class,'destroy'])->name('userDestroy');
 // Tugas
 Route::get('tugas',[TugasController::class,'index'])->name('tugas');
 });
