0dd0daa33315039fc03f5380e3624b2bed381b83
Anton <muhammadanton340@gmail.com>
2025-08-13 19:30:43 +0700

    Export PDF Data

--- app/Http/Controllers/TugasController.php
+++ app/Http/Controllers/TugasController.php
@@ -86,15 +86,4 @@ class TugasController extends Controller
         $user->save();
         return redirect()->route('tugas')->with('success','Data Berhasil Di Hapus');
     }
-     public function pdf (){
-        date_default_timezone_set('Asia/Jakarta'); 
-        $filename = now()->format('d-m-Y_H.i.s');
-        $data = array(
-            'tugas' => Tugas::with('user')->get(),
-            'tanggal' => now()->format('d-m-Y'),
-            'jam'     => now()->format('H.i.s'),
-        );
-        $pdf = Pdf::loadView('admin/tugas/pdf', $data);
-    return $pdf->setPaper('a4', 'landscape')->stream('DataTugas_'.$filename.'.pdf');
-    }
 }
