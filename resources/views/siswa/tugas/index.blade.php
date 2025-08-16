cf6d5d77edbbba88c163d0a8b2753abffca0a46d
Rafit ken sugi <rafitkensugi21@gmail.com>
2025-08-16 08:30:17 +0700

    Export pdf Role siswa

--- resources/views/siswa/tugas/index.blade.php
+++ resources/views/siswa/tugas/index.blade.php
@@ -11,14 +11,80 @@
             <div class="mb-1 mr-2">
             </div>
             <div>
-                <a href="#" class="btn btn-sm btn-danger" target='__blank'>
+                @if (auth()->user()->is_tugas == true)
+                <a href="{{ route('tugasPdf') }}" class="btn btn-sm btn-danger" target='__blank'>
                     <i class="fas fa-file-pdf mr-2"></i>
                     Pdf
                 </a>
+                @endif
+                
             </div>
         </div>
         <div class="card-body">
-            <h1>HALAMAN SISWA</h1>
+            @if (auth()->user()->is_tugas == true)
+                <div class="row">
+                    <div class="col-6">
+                        Nama
+                    </div>
+                    <div class="col-6">
+                        : {{ $tugas->user->nama }}
+                    </div>
+                </div>
+ 
+                <div class="row">
+                    <div class="col-6">
+                        Email
+                    </div>
+                    <div class="col-6">
+                        :
+                        <span class="badge badge-primary">
+                            {{ $tugas->user->email }}
+                        </span> 
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-6">
+                        Tugas
+                    </div>
+                    <div class="col-6">
+                        : {{ $tugas->tugas }}
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-6">
+                        Tanggal Mulai
+                    </div>
+                    <div class="col-6">
+                        :
+                        <span class="badge badge-info">
+                            {{ $tugas->tanggal_mulai }}
+                        </span> 
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-6">
+                        Tanggal Selesai
+                    </div>
+                    <div class="col-6">
+                        :
+                        <span class="badge badge-info">
+                            {{ $tugas->tanggal_selesai }}
+                        </span> 
+                    </div>
+                </div>
+
+            @else
+                <div class="row">
+                    <div class="col-6">
+                        <span class="badge badge-danger">
+                            Belum Ditugaskan
+                        </span>
+                    </div>
+                </div>
+            @endif
         </div>
     </div>
 @endsection
